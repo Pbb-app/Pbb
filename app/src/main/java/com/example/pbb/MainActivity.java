883a75ad362521590562ba9774d738dde449f69f
SAKSHAM-SUDO <54800729+SAKSHAM-SUDO@users.noreply.github.com>
2020-12-20 12:24:10 +0530

    Adventrous Trips updated layout

--- app/src/main/java/com/example/pbb/MainActivity.java
+++ app/src/main/java/com/example/pbb/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity
     private RecyclerView recyclerView;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
