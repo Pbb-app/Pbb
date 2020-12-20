41d1d65593e6ec6de4b368c7c430a29a78e74362
SAKSHAM-SUDO <54800729+SAKSHAM-SUDO@users.noreply.github.com>
2020-12-20 16:54:55 +0530

    Adventrous Trips

--- app/src/main/java/com/example/pbb/MainActivity.java
+++ app/src/main/java/com/example/pbb/MainActivity.java
@@ -13,16 +13,16 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity
 {
-    private String[] tour={"Kedarnath Tour","The Rajgundha 360 Trek - BIR","The Kareri Lake Trek","Kasol - Grahan - Tosh"};
-    private  int[] images={R.drawable.a,R.drawable.b,R.drawable.c,R.drawable.a};
-    private String[] days={"1 Days ,1 Nights","2 Days , 2 Nights","3 Days , 3 Nights","4 Days , 4 Nights"};
-    private String [] monthstart={"oct - nov","feb-dec","dec-feb","mar-apr"};
-    private String[] takeoff={"Delhi","Mumbai","Kolkata","Chennai"};
-    private String[] land={"Kedarnath","Rajgundha","Mcleodganj","Kasol"};
+    private String[] tour={"Kedarnath Tour","The Rajgundha 360 Trek - BIR","The Kareri Lake Trek","Kasol - Grahan - Tosh"};//};
+    private  int[] images={R.drawable.a,R.drawable.b,R.drawable.c,R.drawable.a};//};
+    private String[] days={"1 Days ,1 Nights","2 Days , 2 Nights","3 Days , 3 Nights","4 Days , 4 Nights"};//};
+    private String [] monthstart={"oct - nov","feb-dec","dec-feb","mar-apr"};//};
+    private String[] takeoff={"Delhi","Mumbai","Kolkata","Chennai"};//};
+    private String[] land={"Kedarnath","Rajgundha","Mcleodganj","Kasol"};//};
 
     private List<persons> personsList= new ArrayList<>();
     private RecyclerView recyclerView;
-
+    int count =0;
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,26 +30,22 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         recyclerView=findViewById(R.id.rvw);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        prepareTheList();
+            prepareTheList();
+            count++;
         recyclerAdapter adapter= new recyclerAdapter(personsList);
         recyclerView.setAdapter(adapter);
+
     }
     private void prepareTheList()
     {
-        int count =0;
+       int count =0;
         for(String tour :tour)
         {
             persons person =new persons(tour,images[count],days[count],monthstart[count],takeoff[count],land[count]);
             personsList.add(person);
             count++;
         }
-//        for(int i =0;i<tour.length;i++)
-//        {
-//
-//            persons person=new persons(tour[i],images[i],days[i],monthstart[i],takeoff[i],land[i]);
-//            personsList.add(person);
-//        }
+        }
 
     }
 
-}
